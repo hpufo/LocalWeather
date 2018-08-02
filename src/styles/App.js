667aaeab48d787ec1392a6d4f95e0af4fb910a0b
cshiveio <hpufo2@gmail.com>
2018-08-02 09:15:38 -0400

    added styles

--- src/styles/App.js
+++ src/styles/App.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 export const AppDiv = styled.div`
+  height: 100%;
   text-align: center;
+  background-color: lightcyan;
 `;
 AppDiv.displayName = 'AppDiv';
 export const Header = styled.h1`
+margin-bottom: .5em;
 `;
 Header.displayName = 'Header';
 export const Message = styled.p`
--- src/styles/Day.js
+++ src/styles/Day.js
@@ -1,18 +1,47 @@
 import styled from 'styled-components';
 
+let fontSize= '1.5em';
+
 export const DayDiv = styled.div`
-  display: inline-block
-  `;
+display: inline-block;
+padding: 0 1em 1em 1em;
+&:not(:first-child){
+  border-left: 1px solid black;
+}
+`;
 DayDiv.displayName = 'DayDiv';
-export const CalDate = styled.div``;
+
+export const CalDate = styled.div`
+font-size: ${fontSize};
+`;
 CalDate.displayName = 'CalDate';
-export const DayOfWeek = styled.div``;
+
+export const DayOfWeek = styled.div`
+font-size: ${fontSize};
+`;
 DayOfWeek.displayName = 'DayOfWeek';
-export const Condition = styled.div``;
+
+export const Condition = styled.div`
+font-size: ${fontSize};
+img{
+  width: 100%;
+  height: auto;
+}
+`;
 Condition.displayName = 'Condition';
-export const High = styled.div``;
+
+export const High = styled.div`
+font-size: ${fontSize};
+`;
 High.displayName = 'High';
-export const Temp = styled.div``;
+
+export const Temp = styled.div`
+font-size: ${fontSize};
+`;
 Temp.displayName = 'Temp';
-export const Low = styled.div``;
+
+export const Low = styled.div`
+font-size: ${fontSize};
+color: lightslategrey;
+`;
 Low.displayName = 'Low';
